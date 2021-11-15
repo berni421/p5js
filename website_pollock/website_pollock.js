7bfd509ee12bb797abc443972704bfeef8d217a2
Bernie Elbourn <benri421@gmail.com>
2021-11-15 17:58:02 +0000

    add drag and spill

--- website_pollock/website_pollock.js
+++ website_pollock/website_pollock.js
@@ -1,29 +1,32 @@
+const MAXX = 512;
+const MAXY = MAXX;
+
+
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    createCanvas(MAXX, MAXY);
     background("black");
 }
 
 function splat(x, y) {
-    // the first blob of paint
-    red = random(0, 255);
-    green = random(0, 255);
-    blue = random(0, 255);
-    width = random(0, 10);
-    height = random(0, 10);
+    // blob of paint
+    var red = random(255);
+    var green = random(255);
+    var blue = random(255);
+    var width = random(100);
+    var height = random(100);
+    var seedx = width / 4;
+    var seedy = height / 4;
+    var dx, dy;
+    var dwidth, dheight;
 
     fill(red, green, blue);
-    drag(x, y, width, height)
-}
-
-function drag(x, y, width, height) {
-    // drag the blob of paint
-    dx = random(0 - width, width);
-    dy = random(0 - height, height);
-    dwidth = random(0 - width, width);
-    dheight = random(0 - height, height);
+    noStroke();
     for (var i = 0; i < 10; i++) {
-        noStroke();
         ellipse(x, y, width, height);
+        dx = random(0 - seedx, seedx);
+        dy = random(0 - seedy, seedy);
+        dwidth = random(0 - seedx, seedx);
+        dheight = random(0 - seedy, seedy);
         x = x + dx;
         y = y + dy;
         width = width + dwidth;
@@ -31,13 +34,58 @@ function drag(x, y, width, height) {
     }
 }
 
+function spill(x, y) {
+    // drops of paint
+    var red = random(255);
+    var green = random(255);
+    var blue = random(255);
+    var width = random(10);
+    var height = random(10);
+    var spread = 1.5;
+
+    fill(red, green, blue);
+    noStroke();
+    for (var i = 0; i < 20; i++) {
+        ellipse(x, y, width, height);
+        var dx = random(-1 * spread * width, spread * width);
+        var dy = random(-1 * spread * height, spread * height);
+        x = x + dx;
+        y = y + dy;
+    }
+}
+
+function drag(x, y) {
+    // thin line
+    var red = random(255);
+    var green = random(255);
+    var blue = random(255);
+    var width;
+    var length = random(100);
+    var dx, dy;
+
+    stroke(red, green, blue);
+    for (var i = 0; i < 10; i++) {
+        dx = random(-1 * length, length);
+        dy = random(-1 * length, length);
+        width = random(4, 10);
+        strokeWeight(width);
+        line(x, y, x + dx, y + dy);
+        x = x + dx;
+        y = y + dy;
+    }
+}
 
 function draw() {
-    var x = random(0, windowWidth);
-    var y = random(0, windowHeight);
+    var x = random(0, MAXX);
+    var y = random(0, MAXY);
     splat(x, y);
-
-    if (frameCount >= 1000) {
+    x = random(0, MAXX);
+    y = random(0, MAXY);
+    drag(x, y);
+    x = random(0, MAXX);
+    y = random(0, MAXY);
+    spill(x, y);
+    if (frameCount >= 100) {
         noLoop();
     }
 }
