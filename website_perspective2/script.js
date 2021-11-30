7630336d846da67abf1a2b60b836d94d4afce8dc
Bernie Elbourn <benri421@gmail.com>
2021-11-30 11:17:52 +0000

    add apache dev config and perspective2

--- website_perspective2/script.js
+++ website_perspective2/script.js
@@ -1,6 +1,6 @@
 
 var img;
-var step = 200;
+var step = 400;
 function preload() {
     img = loadImage('../images/rusty-side.jpg');
 }
@@ -8,22 +8,35 @@ function preload() {
 function setup() {
     createCanvas(windowWidth, windowHeight, WEBGL);
 
-}
-
-function draw() {
     background(0);
     perspective(PI / 2, width / height);
     noStroke();
     texture(img);
     translate(-width, 0, 0);
+
     for (var i = 0; i < width * 2 / step; i++) {
         push();
-        translate(step * i, 0, 0);
-        rotateX(frameCount * -0.01);
-        rotateY(frameCount * 0.01);
-        rotateZ(frameCount * 0.01);
+        translate(step * i, 0, -width / 1.1);
+        rotateY(PI / 2);
         plane(width / 1.1, height / 1.1, 24, 1, false);
         pop();
+        if (i % 2 == 0) {
+            push();
+            translate(step * i, 0, -2 * width / 1.1);
+            rotateY(PI);
+            plane(step, height / 1.1, 24, 1, false);
+            pop();
+        }
+        //  else {
+        //     translate(0, 0, -step);
+        //     rotateY(PI / 2);
+        //     plane(step, height / 1.1, 24, 1, false);
+        // }
+        pop();
     }
-    //exit(1);
+
+}
+
+function draw() {
+    rotateY(frameCount * 0.01);
 }
