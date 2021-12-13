6f7df18c826d0cbbde5e8ae87fbbfc16eada95c4
Bernie Elbourn <benri421@gmail.com>
2021-12-13 22:36:51 +0000

    grey cloud

--- website_cloud/script.js
+++ website_cloud/script.js
@@ -1,43 +1,33 @@
 var size;
-var rn = 100;
-var bn = 200;
-var gn = 300;
+var objectsPerRow;
+var greyn;
 
 function preload() {
 }
 
 function setup() {
     createCanvas(windowWidth, windowHeight, WEBGL);
-    size = windowWidth / 20;
-    background(0);
-    scale(size);
-    noStroke();
+    objectsPerRow = 60;
+    size = windowWidth / objectsPerRow;
+    greyn = map(random(), 0, 1, 0, 1000);
     frameRate(10);
+    noStroke();
     //noLoop();
 }
 
 function draw() {
     background(0);
-    //pointLight(150, 150, 150, 0, -2 * height, 2 * (width + height));
-    //directionalLight(255, 255, 255, 0, height, width + height);
     ambientLight(255);
     rotateY(frameCount * 0.1);
-    //rotateX(frameCount * 0.1);
     for (var x = -width / 2; x < width / 2; x = x + size) {
         for (var y = -height / 2; y < height / 2; y = y + size) {
             push();
             translate(x, y, 0);
-            var red = 255 * noise(rn);
-            var blue = 255 * noise(bn);
-            var green = 255 * noise(gn);
-            ambientMaterial(red, green, blue);
-            //specularMaterial(red, green, blue);
+            grey = 255 * noise(greyn);
+            ambientMaterial(grey);
             box(size);
-            rn = rn + 0.1;
-            bn = bn + 0.1;
-            gn = gn + 0.1;
+            greyn = greyn + 0.1;
             pop();
         }
-
     }
 }
