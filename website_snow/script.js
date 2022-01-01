37c1dcfbb309e7100a1570ea1468e8ccf06c5ac6
Bernie Elbourn <benri421@gmail.com>
2022-01-01 21:01:04 +0000

    allow snow to settle

--- website_snow/script.js
+++ website_snow/script.js
@@ -1,4 +1,5 @@
-var flake;
+var flakes = [];
+var ground;
 
 function preload() {
 }
@@ -6,25 +7,61 @@ function preload() {
 function setup() {
     createCanvas(windowWidth, windowHeight, WEBGL);
     ambientLight(255); // white light
-    flake = new Flake();
+    //camera(0, height, width, 0, 0, 0, 0, 1, 0);
 }
 
 function draw() {
     background(0);
-    flake.display();
+
+    for (i = 0; i < flakes.length; i++) {
+        flakes[i].display();
+        flakes[i].update();
+
+        // Clean up lost flake and replace
+        if (flakes[i].lost()) {
+            flakes.splice(i, 1);
+
+        }
+    }
+
+    // add flakes until some time
+    if (frameCount < 50 * 10) {
+        flakes.push(new Flake());
+    }
+
+    // stop animation after a time
+    if (frameCount > 50 * 20) {
+        noLoop();
+    }
 }
-
-
 
 class Flake {
     constructor() {
-        this.position = createVector(0, 0, 0);
-        this.velocity = createVector(random(-1, 1), random(1), random(1));
-        this.acceleration = createVector(0, 1, 0);
-        this.size = 16;
+        this.position = createVector(random(-width / 2, width / 2), -height / 2, 0);
+        this.velocity = createVector(random(0.01), random(1), random(-0.01, 0.01));
+        this.acceleration = createVector(random(0.01), random(0.1), random(-0.1, 0.1));
+        this.size = 4;
     }
 
     update() {
+        this.position.add(this.velocity);
+        this.velocity.add(this.acceleration);
+        if (this.position.y < -height / 2 || this.position.y > height / 2) {
+            this.velocity.x = 0;
+            this.velocity.y = 0;
+            this.velocity.z = 0;
+            this.acceleration.x = 0;
+            this.acceleration.y = 0;
+            this.acceleration.z = 0;
+        }
+    }
+
+    lost() {
+        var l = (this.position.x < -width / 2 ||
+            this.position.x > width / 2 ||
+            this.position.z < -width ||
+            this.position.z > width)
+        return l;
     }
 
     display() {
