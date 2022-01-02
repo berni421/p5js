b60009ec7978a523870853f2c5269132a6537c68
Bernie Elbourn <benri421@gmail.com>
2022-01-02 11:41:43 +0000

    add rotation

--- website_snow/script.js
+++ website_snow/script.js
@@ -1,52 +1,68 @@
 var flakes = [];
-var ground;
+var spin = 0.0;
 
 function preload() {
 }
 
 function setup() {
     createCanvas(windowWidth, windowHeight, WEBGL);
-    ambientLight(255); // white light
+    ambientLight('white'); // white light
     //camera(0, height, width, 0, 0, 0, 0, 1, 0);
 }
 
 function draw() {
     background(0);
 
+    // Rotate after some time
+    if (frameCount > 50 * 5) {
+        rotateY(spin);
+        spin += 0.01;
+    }
+
+    push(0);
+    noStroke();
+    fill(0, 0, 50);
+    translate(0, height / 2, 0);
+    rotateX(PI / 2);
+    plane(width * 4);
+    pop();
+
+    // add flakes until some time
+    if (frameCount < 50 * 10) {
+        for (i = 0; i < 5; i++) {
+            flakes.push(new Flake());
+            flakes.push(new Flake());
+        }
+    }
+
     for (i = 0; i < flakes.length; i++) {
         flakes[i].display();
         flakes[i].update();
 
-        // Clean up lost flake and replace
+        // Clean up lost flake
         if (flakes[i].lost()) {
             flakes.splice(i, 1);
-
         }
     }
 
-    // add flakes until some time
-    if (frameCount < 50 * 10) {
-        flakes.push(new Flake());
-    }
-
     // stop animation after a time
-    if (frameCount > 50 * 20) {
-        noLoop();
-    }
+    // if (frameCount > 50 * 20) {
+    //     noLoop();
+    // }
 }
 
 class Flake {
     constructor() {
-        this.position = createVector(random(-width / 2, width / 2), -height / 2, 0);
-        this.velocity = createVector(random(0.01), random(1), random(-0.01, 0.01));
-        this.acceleration = createVector(random(0.01), random(0.1), random(-0.1, 0.1));
-        this.size = 4;
+        this.position = createVector(random(-width / 2, width / 2), -height / 2, random(-width / 2, width / 2));
+        this.velocity = createVector(random(-0.01, 0.01), random(1), random(0.01));
+        this.acceleration = createVector(random(-0.01, 0.01), random(1), random(-0.1, 0.1));
+        this.size = 2;
     }
 
     update() {
         this.position.add(this.velocity);
         this.velocity.add(this.acceleration);
-        if (this.position.y < -height / 2 || this.position.y > height / 2) {
+        if (this.position.y < -height / 2 || this.position.y > height / 2 - 2 * this.size) {
             this.velocity.x = 0;
             this.velocity.y = 0;
             this.velocity.z = 0;
@@ -57,15 +73,15 @@ class Flake {
     }
 
     lost() {
-        var l = (this.position.x < -width / 2 ||
-            this.position.x > width / 2 ||
-            this.position.z < -width ||
-            this.position.z > width)
+        var l = (this.position.x < -width * 2 ||
+            this.position.x > width * 2 ||
+            this.position.z < -width * 2 ||
+            this.position.z > width * 2)
         return l;
     }
 
     display() {
-        stroke(0, 0, 255); //blue
+        stroke('white');
         strokeWeight(4 * this.size);
         point(this.position);
     }
