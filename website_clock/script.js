ba7aae08f534757d0f02ddc08dda9c1f7add80c0
Berni Elbourn <berni421@gmail.com>
2022-01-19 12:13:30 +0000

    refactor and words not numbers

--- website_clock/script.js
+++ website_clock/script.js
@@ -1,25 +1,80 @@
 var myFont;
+var dateTime;
+var dayName = [];
+var dayPeriod = [];
+var dayMonth = [];
 
 function preload() {
-    myFont = loadFont('../fonts/DejaVuSerif.ttf');
 }
 
 function setup() {
-    createCanvas(windowWidth, windowHeight, WEBGL);
-    ambientLight('white'); // white light
+    createCanvas(windowWidth, windowHeight);
     frameRate(1);
-    textFont(myFont);
     textAlign(CENTER, CENTER);
-    textSize(height / 5);
     fill('white')
+
+    dayName[0] = "Sunday";
+    dayName[1] = "Monday";
+    dayName[2] = "Tuesday";
+    dayName[3] = "Wednesday";
+    dayName[4] = "Thursday";
+    dayName[5] = "Friday";
+    dayName[6] = "Saturday";
+
+    for (var i = 0; i < 12; i++) { dayPeriod[i] = "Morning"; }
+    for (var i = 12; i < 18; i++) { dayPeriod[i] = "Afternoon"; }
+    for (var i = 18; i < 24; i++) { dayPeriod[i] = "Evening"; }
+
+    dayMonth[0] = "January";
+    dayMonth[1] = "Febuary";
+    dayMonth[2] = "March";
+    dayMonth[3] = "April";
+    dayMonth[4] = "May";
+    dayMonth[5] = "June";
+    dayMonth[6] = "July";
+    dayMonth[7] = "August";
+    dayMonth[8] = "September";
+    dayMonth[9] = "October";
+    dayMonth[10] = "November";
+    dayMonth[11] = "December";
 }
 
 function draw() {
     background(0);
 
-    var time = hour() + ":" + minute() + ":" + second();
-    var date = day() + "/" + month() + "/" + year();
+    const dateTime = new Date();
 
-    text(time, 0, - height / 4);
-    text(date, 0, + height / 4);
+    const dayOfWeek = dateTime.getDay(); // Sunday - Saturday : 0 - 6
+
+    const month = dateTime.getMonth();
+    const year = dateTime.getFullYear();
+    const day = dateTime.getDate();
+
+    const hour = dateTime.getHours();
+    const minute = dateTime.getMinutes();
+    const second = dateTime.getSeconds();
+
+    var hourPad = "";
+    var minutePad = "";
+    var secondPad = "";
+    var dayPad = "";
+    var monthPad = "";
+
+    if (hour < 10) { hourPad = "0"; }
+    if (minute < 10) { minutePad = "0"; }
+    if (second < 10) { secondPad = "0"; }
+    if (day < 10) { dayPad = "0"; }
+
+    var time = hourPad + hour + ":" +
+        minutePad + minute + ":" +
+        secondPad + second;
+    var date = dayPad + day + " " +
+        monthPad + dayMonth[month] + " " +
+        year;
+
+    textSize(width * 0.1);
+    text(time, width / 2, height * 0.25);
+    textSize(width * 0.075);
+    text(dayName[dayOfWeek] + " " + dayPeriod[hour], width / 2, height * 0.5);
+    text(date, width / 2, height * 0.75);
 }
