b3402033417c6d18ffe3c165e40827ed436d6f4f
Berni Elbourn <berni421@gmail.com>
2022-02-25 08:40:47 +0000

    bugs ditch fetch and use loadImage in preload

--- website_fetch/script.js
+++ website_fetch/script.js
@@ -1,27 +1,31 @@
-function preload() {}
-
-async function setTexture() {
-  const response = await fetch('../images/rusty.jpg');
-  print(response);
-  const blob = await response.blob();
-  print(blob);
-  const o = URL.createObjectURL(blob);
-  print(o);
-  const i = await createImg(o, "rusty");
-  i.hide();
-  print(i);
-  texture(i);
+var img;
+function preload() {
+  img = loadImage('../images/rusty.jpg');
 }
+
+// async function setTexture() {
+//   const response = await fetch('../images/rusty.jpg');
+//   print(response);
+//   const blob = await response.blob();
+//   print(blob);
+//   const o = URL.createObjectURL(blob);
+//   print(o);
+//   const i = await createImg(o, "rusty");
+//   i.hide();
+//   print(i);
+//   texture(i);
+// }
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
-  setTexture()
-    .then(response => {
-      print("OK");
-    })
-    .catch(error => {
-      print("Error:", error);
-    });
+  // setTexture()
+  //   .then(response => {
+  //     print("OK");
+  //   })
+  //   .catch(error => {
+  //     print("Error:", error);
+  //   });
+  texture(img);
 }
 
 function draw() {
