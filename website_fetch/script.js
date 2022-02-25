f5c3efd70135321b229dbfcea99559680671bc6b
Bernie Elbourn <benri421@gmail.com>
2022-02-25 17:14:18 +0000

    speed up cat movement

--- website_fetch/script.js
+++ website_fetch/script.js
@@ -1,32 +1,12 @@
 var img;
-var speed = 0.001;
+var speed = 0.01;
 
 function preload() {
   img = loadImage('../images/rusty.jpg');
 }
 
-// async function setTexture() {
-//   const response = await fetch('../images/rusty.jpg');
-//   print(response);
-//   const blob = await response.blob();
-//   print(blob);
-//   const o = URL.createObjectURL(blob);
-//   print(o);
-//   const i = await createImg(o, "rusty");
-//   i.hide();
-//   print(i);
-//   texture(i);
-// }
-
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
-  // setTexture()
-  //   .then(response => {
-  //     print("OK");
-  //   })
-  //   .catch(error => {
-  //     print("Error:", error);
-  //   });
   texture(img);
 }
 
