3836baad1cd650b14e9489f415d6a9a66af10898
Bernie Elbourn <benri421@gmail.com>
2022-03-13 12:08:21 +0000

    add setup --- slow

--- website_soduko/grid.js
+++ website_soduko/grid.js
@@ -2,10 +2,23 @@ class Grid {
   constructor() {
     this.grid = [];
     this.avail = [];
+    this.cells = [];
     for (var row = 0; row < 9; row++) {
       this.grid[row] = [];
+      this.cells[row] = [];
       for (var column = 0; column < 9; column++) {
-        this.grid[row][column] = null;
+        const button = createButton(" ");
+        button.hide();
+        this.grid[row][column] = {
+          button: button,
+          value: null
+        };
+        const buttonCell = createButton(" ");
+        buttonCell.hide();
+        this.cells[row][column] = {
+          button: buttonCell,
+          value: null
+        };
       }
     }
     for (var n = 0; n < 9; n++) {
@@ -13,41 +26,112 @@ class Grid {
     }
   }
 
-  solve() {
-    var a = 0;
-    while (a < 128 && this.add()) {
-      a++;
-    };
-    print("attempts:", a);
+  prepGrid() {
+    for (var row = 0; row < 9; row++) {
+      for (var column = 0; column < 9; column++) {
+        this.grid[row][column].value = this.cells[row][column].value;
+      }
+    }
+  }
+
+  displaySolveButton() {
+    const s = 24;
+    textSize(s * 2);
+    const button = createButton();
+    button.html("SOLVE ME");
+    button.position(8 * s, 12 * s);
+    button.size(AUTO, s);
+    button.mousePressed(() => this.solveButton(this));
+  }
+
+  cloneGrid(g) {
+    const n = [];
+    for (var row = 0; row < 9; row++) {
+      n[row] = [];
+      for (var column = 0; column < 9; column++) {
+        n[row][column] = {
+          button: g[row][column].button,
+          value: g[row][column].value
+        };
+      }
+    }
+    return n;
+  }
+
+  cloneAvail(a) {
+    const n = [];
+    for (var i = 0; i < 9; i++) {
+      n[i] = a[i];
+    }
+    return n;
+  }
+
+  solveButton() {
+    this.prepGrid();
+    var finished = false;
+    while (!finished) {
+      const sg = this.cloneGrid(this.grid);
+      const sa = this.cloneAvail(this.avail);
+      if (this.solve(128)) {
+        finished = true;
+      } else {
+        //backtrack
+        this.displayResultCells();
+        this.grid = this.cloneGrid(sg);
+        this.avail = this.cloneAvail(sa);
+        //const skipped = this.makeChoice();
+      }
+    }
+    background("black");
+    this.displayTitles();
+    this.displayInitCells();
+    if (finished) {
+      this.displayResultCells();
+    } else {
+      this.displayFailed();
+    }
+  }
+
+  solve(n) {
+    if (n < 0) {
+      // not finnished in steps available, backtrack
+      return false;
+    }
+    const index = this.nextAvailable();
+    if (index == null) {
+      //finished
+      return true;
+    }
+    var choiceMade = false;
+    var value;
+    for (var i = 0; i < 9; i++) {
+      value = this.makeChoice();
+      if (this.isValid(value, index)) {
+        this.grid[index.row][index.column].value = value;
+        choiceMade = true;
+        break;
+      }
+    }
+    if (choiceMade) {
+      return this.solve(n - 1);
+    } else {
+      // backtrack
+      print("backtrack");
+      return false;
+    }
   }
 
   makeChoice() {
-    const c = this.avail.shift();
-    this.avail.push(c);
-    return c;
-  }
-
-  add() {
-    const index = this.nextAvailable();
-    if (index == null) {
-      return false;
-    }
-    const {
-      row,
-      column
-    } = index;
-    const value = this.makeChoice();
-    if (this.isValid(value, index)) {
-      this.grid[row][column] = value;
-      return true;
-    }
-    return this.add();
+    const choice = this.avail.shift();
+    this.avail.push(choice);
+    return choice;
   }
 
   nextAvailable() {
     for (var row = 0; row < 9; row++) {
       for (var column = 0; column < 9; column++) {
-        if (this.grid[row][column] == null) {
+        const value = this.grid[row][column].value;
+        if (value == null) {
           const index = {
             row: row,
             column: column
@@ -56,52 +140,109 @@ class Grid {
         }
       }
     }
+    // finished
     return null;
   }
 
   isValid(value, index) {
-    //print("isValid: ", index.row, index.column, value);
     const tileRow = 3 * floor(index.row / 3);
     const tileColumn = 3 * floor(index.column / 3);
     for (var row = tileRow; row < tileRow + 3; row++) {
       for (var column = tileColumn; column < tileColumn + 3; column++) {
-        if (this.grid[row][column] == value) {
-          //print("tile check FAILED");
+        if (this.grid[row][column].value == value) {
           return false;
         }
       }
     }
-    // value not in row
-    var row = index.row;
     for (var column = 0; column < 9; column++) {
-      if (this.grid[row][column] == value) {
+      if (this.grid[index.row][column].value == value) {
         return false;
       }
     }
-    // value not in column
-    var column = index.column;
     for (var row = 0; row < 9; row++) {
-      if (this.grid[row][column] == value) {
+      if (this.grid[row][index.column].value == value) {
         return false;
       }
     }
     return true;
   }
 
-  display(offset) {
+  updateCell(cell) {
+    var {
+      row,
+      column
+    } = cell;
+    var value = this.cells[row][column].value;
+    if (value == null) {
+      value = 1
+    } else if (value == 9) {
+      value = null;
+    } else {
+      value++;
+    }
+    this.cells[row][column].value = value;
+    const button = this.cells[row][column].button;
+    button.html(value);
+  }
+
+  displayTitles() {
+    const s = 24
+    textSize(s);
+    text("Initial Grid", s * 0, s * 1);
+    text("Solved Grid", s * 12, s * 1);
+    text("click the initial grid cells to load initial pattern", s * 0, s * 14);
+  }
+
+  displayInitCells() {
     const s = 24;
-    var t, v;
     textSize(s);
     for (var row = 0; row < 9; row++) {
       for (var column = 0; column < 9; column++) {
-        v = this.grid[row][column];
-        if (v == null) {
-          t = "0";
-        } else {
-          t = v;
+        var valueText = this.cells[row][column].value;
+        if (valueText == null) {
+          valueText = " ";
         }
-        text(t, offset * s + s * column, 100 + s * row);
+        const button = this.cells[row][column].button;
+        button.html(valueText);
+        button.position(column * s, 2 * s + row * s);
+        button.size(s, s);
+        const cellInfo = {
+          me: this,
+          row: row,
+          column: column
+        };
+        button.mousePressed(() => this.updateCell(cellInfo));
+        button.show();
       }
     }
+  }
+
+  displayResultCells() {
+    const s = 24;
+    textSize(s);
+    for (var row = 0; row < 9; row++) {
+      for (var column = 0; column < 9; column++) {
+        const value = this.grid[row][column].value;
+        const button = this.grid[row][column].button;
+        button.html(value);
+        button.position(12 * s + column * s, 2 * s + row * s);
+        button.size(s, s);
+        button.show();
+      }
+    }
+  }
+
+  displayFailed() {
+    const s = 24;
+    textSize(s * 2);
+    text("Unsolved!", s * 24, s * 6);
+    //hide result buttons
+    for (var row = 0; row < 9; row++) {
+      for (var column = 0; column < 9; column++) {
+        const button = this.grid[row][column].button;
+        //button.hide();
+      }
+    }
+    this.displayResultCells();
   }
 }
--- website_soduko/script.js
+++ website_soduko/script.js
@@ -1,23 +1,15 @@
-var g; // the Grid
+var myGrid; // the Grid
 
 function preload() {}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background("black");
   fill("white");
-  g = new Grid();
-
-  // set some values
-  g.grid[0][0] = 9;
-
-  g.display(1);
-
-  g.solve();
-
-  print(g.grid);
-
-  g.display(11);
+  background("black");
+  myGrid = new Grid();
+  myGrid.displayTitles();
+  myGrid.displayInitCells();
+  myGrid.displaySolveButton();
 }
 
 function draw() {
